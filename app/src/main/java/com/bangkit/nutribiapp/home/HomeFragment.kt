1d96be1b6430b1a05fb477928a8247076e11e586
unknown <aransafitraprawira@ymail.com>
2021-05-25 18:09:34 +0700

    add home, ingredient, detail

---
+++ app/src/main/java/com/bangkit/nutribiapp/home/HomeFragment.kt
@@ -0,0 +1,22 @@
+package com.bangkit.nutribiapp.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.bangkit.nutribiapp.R
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+
+}
---
+++ app/src/main/java/com/bangkit/nutribiapp/ingredient/IngredientActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.nutribiapp.ingredient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.nutribiapp.R
+
+class IngredientActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ingredient)
+    }
+}
