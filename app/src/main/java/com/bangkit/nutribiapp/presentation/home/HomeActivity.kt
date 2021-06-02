ebb4c8e06307b01813926aa8b65154b3a8e21b0b
unknown <aransafitraprawira@ymail.com>
2021-06-02 19:53:40 +0700

    add intent & modify intent-filter

--- app/src/main/java/com/bangkit/nutribiapp/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/bangkit/nutribiapp/presentation/home/HomeActivity.kt
@@ -1,8 +1,11 @@
 package com.bangkit.nutribiapp.presentation.home
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bangkit.nutribiapp.databinding.ActivityHomeBinding
+import com.bangkit.nutribiapp.presentation.ingredient.IngredientActivity
+import com.bangkit.nutribiapp.presentation.objectdetection.ObjectDetection
 
 class HomeActivity : AppCompatActivity() {
 
@@ -14,6 +17,15 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.btnScan.setOnClickListener {
+            val intent = Intent(this@HomeActivity, ObjectDetection::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnSearch.setOnClickListener {
+            val intent = Intent(this@HomeActivity, IngredientActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
